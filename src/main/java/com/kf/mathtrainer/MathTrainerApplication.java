727d8d181610f9888a7d425d21941731a82f1f2a
Kai Feldhoff <kaifeldhoff@yahoo.de>
2025-07-14 15:37:26 +0200

    spring-initializer

---
+++ src/main/java/com/kf/mathtrainer/MathTrainerApplication.java
@@ -0,0 +1,13 @@
+package com.kf.mathtrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathTrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathTrainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kf/mathtrainer/MathTrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kf.mathtrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathTrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
